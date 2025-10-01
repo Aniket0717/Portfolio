dd4c5226771ec7214923528ffc8d1d3545d9c208
Aniket0717 <anikettalpe111@gmail.com>
2025-10-01 06:27:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//smooth scrolling
+document.querySelectorAll(a [href^="#"]). forEach
+(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute
+            ('hred')).scrollIntoview({
+                behavior: 'smooth'
+            });
+    });})
